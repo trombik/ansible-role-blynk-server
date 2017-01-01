849e30ceaeab7d279de4e2fb9e991f33a68347cf
Tomoyuki Sakurai <tomoyukis@reallyenglish.com>
2017-01-01 15:37:29 +0900

    add simple server tests

--- tests/integration/local_server/spec/default_spec.rb
+++ tests/integration/local_server/spec/default_spec.rb
@@ -3,26 +3,27 @@ require 'spec_helper'
 class ServiceNotReady < StandardError
 end
 
-sleep 10 if ENV['JENKINS_HOME']
-
 context 'after provisioning finished' do
-
   describe server(:client1) do
-
     it 'should be able to ping server' do
       result = current_server.ssh_exec("ping -c 1 #{ server(:server1).server.address } && echo OK")
       expect(result).to match(/OK/)
     end
-
   end
 
   describe server(:server1) do
-
     it 'should be able to ping client' do
       result = current_server.ssh_exec("ping -c 1 #{ server(:client1).server.address } && echo OK")
       expect(result).to match(/OK/)
     end
-
   end
 
+  describe server(:server1) do
+    describe capybara("https://192.168.21.200:7443") do
+      it "shows admin page" do
+        visit "/admin#/dashboard"
+        expect(page).to have_title "Blynk Administration"
+      end
+    end
+  end
 end
